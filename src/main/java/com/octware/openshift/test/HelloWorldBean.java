6c8dbc27ac46c724e1479de48eb29d252341b5e4
subhakantagithub <33386065+subhakantagithub@users.noreply.github.com>
2022-01-14 15:49:45 -0500

    Add files via upload

---
+++ src/main/java/com/octware/openshift/test/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.octware.openshift.test;
+
+public class HelloWorldBean {
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	private String message;
+	
+	
+}
---
+++ src/test/java/com/octware/openshift/test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octware.openshift.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
